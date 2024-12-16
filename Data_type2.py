320b2e49fef991e6f28247c45f231e9f308d3846
Afzal khan <afzalkhan01765@gmail.com>
2024-12-16 13:05:06 +0600

    add more file

---
+++ Data_type2.py
@@ -0,0 +1,26 @@
+
+"""
+    Bool = True
+    bool = False
+    print(type(Bool))
+    print(type(bool))
+    print(int(Bool))
+    print(int(bool))
+    
+"""
+set1 = {1,2,3,4,5,6}
+print(set1)
+set1.add(7)
+print(set1)
+
+set1.remove(1)
+print(set1)
+# set1.clear()
+print(set1)
+set1.add(4)
+print(set1)
+
+
+
+
+
---
+++ Python_Data_Type.py
@@ -0,0 +1,10 @@
+a =int(input()) #form user input
+b =int(input())
+print("The sumation value is :",a+b)
+print("The multiplication value is :",a - b)
+print("The divition value is :",a /b)
+print(type(a))
+b = 4.33
+print(type(b))
+name ="Afzal"
+print(name)
