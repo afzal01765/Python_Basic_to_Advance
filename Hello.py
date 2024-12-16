47f514c11e9325a78fc2a8ee061d6b2c2339a20d
Afzal khan <afzalkhan01765@gmail.com>
2024-12-16 12:43:45 +0600

    comment

--- Hello.py
+++ Hello.py
@@ -1,4 +1,4 @@
 print("This is my First code  on python") #Sting
 print(23333) #integer code run
 print(54.4344) #folating point number
-print("!@#$%^&*****(()*&^%$")  #special character  
+print("!@#$%^&*****(()*&^%$")  #special character
---
+++ python_comment.py
@@ -0,0 +1,7 @@
+print("Hell world")
+print("This is my secound code for comment")  #single line comment
+"""
+this is my multiline comment this is my 
+multiline comment this is my multiline comment
+f"Welcome {session['user']}! <a href='/logout'>Logout</a>"
+"""
