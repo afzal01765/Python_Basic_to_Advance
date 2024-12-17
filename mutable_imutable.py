b5ff57c6e18891b8e08283c698d4e1fa057f4122
Afzal khan <afzalkhan01765@gmail.com>
2024-12-17 17:22:33 +0600

    add file

---
+++ mutable_imutable.py
@@ -0,0 +1,32 @@
+my_list = [1,2,3,4,5]
+
+print(my_list)
+my_list[2] = 1
+print(my_list)
+my_list.append(3)
+my_list.pop()
+my_list.pop(2)
+print(my_list)
+my_list.remove(5)
+my_list.remove(1)
+# my_list.remove(3)
+my_list.reverse()
+my_list.clear()
+print(my_list)
+
+#পরিবর্তন করা গেলে মিওটেবল না গেলে ইমিউটেবল  list ,dictionary , set ইত্যাদি মিউটেবল
+# অন্যদিগে string ,tuple , number ইত্যাদি ইমিউটেবল ডাটাটাইপ ।
+
+
+
+
+
+
+
+
+
+
+
+
+
+
